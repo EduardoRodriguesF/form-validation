71426bc931408790dbf70525dfd3dde6bc9d73f0
EduardoRodriguesF <eduaadorfernandes@gmail.com>
2020-10-24 16:13:49 -0300

    fix: mask not adjusting when user did by half

--- public/src/index.js
+++ public/src/index.js
@@ -1,10 +1,10 @@
 // Phone mask
 document.querySelector('#phone input').addEventListener('input', function(e) {
     const num = e.target.value;
-    const phoneAdjust = num.replace(/(\d{2})(\d{4,5})(\d{4})/, 
+    const phoneAdjust = num.replace(/\(*(\d{2})\)*(\d{4,5})-*(\d{4})/, 
         function(regex, DDD, num1, num2) {
             return `(${DDD}) ${num1}-${num2}`;
         });
 
     e.target.value = phoneAdjust
-})
+});
